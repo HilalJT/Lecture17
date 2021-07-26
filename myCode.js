0c995c3dc048d7d3cda2279c5030e9b351459a22
Hilola <tursun.hilola@gmail.com>
2021-07-26 18:21:18 -0500

    Updated

--- myCode.js
+++ myCode.js
@@ -1,2 +1,15 @@
 window.addEventListener('load', function() {
+
+    fetch("https://handlers.education.launchcode.org/static/weather.json").then(function(response) {
+        response.json().then(function(jsonResponse){  
+            let myWeatherDiv = document.getElementById('weather-output');
+            myWeatherDiv.innerHTML = `
+            </h3>${jsonResponse.status}<h3>
+            <p>Temp: ${jsonResponse.temp} F</p>
+            <p>Low/High: ${jsonResponse.tempMin} F / ${jsonResponse.tempMax} F</p>
+            <p>Wind Speed: ${jsonResponse.windSpeed} mph</p>`
+        });
+        
+    });
+
 });
